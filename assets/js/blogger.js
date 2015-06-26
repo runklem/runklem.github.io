bf7d126d349a07c36ee1c6611826dfc713ba9ce8
Matthew Runkle <mrunkle_he@dev.bse.edu>
2015-06-25 21:19:05 -0500

    working pagination; need to add default posts

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -1,15 +1,57 @@
-function getblog() {
-	$.getJSON('entries.json', function(jd) {
-
-		for (var i = 0; i < jd.entries.length; i++) {
-			var key = i;
-			var date = jd.entries[i].date;
-			var title = jd.entries[i].title;
-			var text = jd.entries[i].text;
-
-			$("#blog").append("<div id=" +key+ " class='col-sm-12 blogpost'><small><p id='date' class='muted' style='float:right;'>" +date+ " </p></small><h5 id='title'>" +title+ " </h5><p id='blogpost'>" +text+ " </p><hr/></div>");
-		}
+function pager(pages) {
+	$("#pag").bootpag({
+		total: pages
+	}).on("page", function(event, num){
+	 	$(".content").html("Page" + num);
 	});
 }
 
-$(document).ready(getblog);
+function getblog(postsPerPage) {
+	//GET JSON
+	$.getJSON('entries.json', function(jd) {
+	
+	//ESTABLISH TOTAL NUMBER OF ENTRIES
+	var entries = [];
+	for (var i = 0; i < jd.entries.length; i++) {
+		var key = i;
+		var date = jd.entries[i].date;
+		var title = jd.entries[i].title;
+		var text = jd.entries[i].text;
+		entries.push(key);
+	}
+
+	//PROGRAMMATICALLY SET NUMBER OF PAGES
+	var pages = Math.floor(entries.length / postsPerPage);
+	pages += 1;
+	
+	//PAGER() INSIDE GETBLOG  ** with content generation inside "pager" code **  TO ACCESS "NUM"
+	$("#pag").bootpag({
+		total: pages
+	}).on("page", function(event, num){
+	 	$(".content").html("Page" + num);
+	 	console.log(num);
+
+	//CLEAR PREVIOUS POSTS
+	$("#blog").empty();
+	
+	//ADD BLOG POSTS
+	var startIndex = (num - 1) * postsPerPage;
+	var finishIndex = startIndex + (postsPerPage - 1);
+
+	for (var i = startIndex; i <= finishIndex; i++) {
+		var key = i;
+		var date = jd.entries[i].date;
+		var title = jd.entries[i].title;
+		var text = jd.entries[i].text;
+
+		$("#blog").append("<div id=" +key+ " class='col-sm-12 blogpost'><small><p id='date' class='muted' style='float:right;'>" +date+ " </p></small><h5 id='title'>" +title+ " </h5><p id='blogpost'>" +text+ " </p><hr/></div>");
+	}
+	});
+
+	pager(pages)
+
+	});
+}
+
+
+$(document).ready(getblog(5));
